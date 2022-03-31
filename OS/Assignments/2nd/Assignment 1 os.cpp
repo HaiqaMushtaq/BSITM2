45042fbe375feace195ab2436cf68a18b35973ef
Haiqa Mushtaq <2020-cs-5214@uosahiwal.edu.pk>
2022-03-31 23:56:12 +0500

    seed labs

---
+++ OS/Assignments/2nd/Assignment 1 os.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    cout<<"Name     Haiqa Mushtaq"<<endl;
+    cout<<"Roll no  BSIT M2-20-13"<<endl;
+    
+    pid_t a = fork();
+    pid_t b = fork();
+    pid_t c = fork();
+    if (a > 0)
+    {
+        wait (NULL);
+        pid_t b = fork();
+   
+    if (b > 0)
+    {
+        wait(NULL);
+        pid_t c = fork();
+    
+     if (c > 0)
+    {
+        wait(NULL);
+        cout<<"Parent Process = "<<endl;
+        cout<<"PID ="<<getpid()<<endl;
+        cout<<"Parent PID = "<<getppid<<endl;
+        cout<<endl;
+    }
+    else if(c == 0)
+    {
+        cout<<"Child 1="<<endl;
+        cout<<"PID ="<<getpid()<<endl;
+        cout<<"Parent PID ="<<getppid()<<endl;
+        cout<<endl;
+    }
+     }
+    else if (b == 0)
+    {
+        cout<<"Child 2 ="<<endl;
+        cout<<"PID ="<<getpid()<<endl;
+        cout<<"Parent PID ="<<getppid()<<endl;
+        cout<<endl;
+    }
+     }
+    else if (a == 0)
+    {
+        cout<<"Child 3 ="<<endl;
+        cout<<"PID ="<<getpid()<<endl;
+        cout<<"Parent PID ="<<getppid()<<endl;
+        cout<<endl;
+    }
+    
+    pid_t e = fork();
+    if(e > 0)
+    {
+    wait(NULL);
+    pid_t f=fork();
+    
+    if(f > 0)
+    {
+    wait(NULL);
+    pid_t g = fork();
+    
+    if(g > 0)
+    {
+    wait(NULL);
+     cout<<"Parent Process = "<<endl;
+     cout<<"PID ="<<getpid()<<endl;
+     cout<<"Parent PID = "<<getppid<<endl;
+     cout<<endl;
+     }
+     else if(g == 0)
+     {
+        cout<<"Child 1="<<endl;
+        cout<<"PID ="<<getpid()<<endl;
+        cout<<"Parent PID ="<<getppid()<<endl;
+        cout<<endl;
+        }
+       	 }
+        else if (f == 0)
+        {
+        cout<<"Child 2 ="<<endl;
+        cout<<"PID ="<<getpid()<<endl;
+        cout<<"Parent PID ="<<getppid()<<endl;
+        cout<<endl;
+        }
+       	 }
+        else if (e == 0)
+        {
+        cout<<"Child 3 ="<<endl;
+        cout<<"PID ="<<getpid()<<endl;
+        cout<<"Parent PID ="<<getppid()<<endl;
+        cout<<endl;
+        }
+    return 0;
+}
